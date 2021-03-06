6c5726df52ec8d1c7d0efb8ec15b8630e9b4190e
Harrison Hong <harrisonhong14@gmail.com>
2021-03-05 18:48:15 -0800

    Finished Working Function

--- Webscrape.py
+++ Webscrape.py
@@ -4,9 +4,9 @@ from selenium import webdriver
 import re
 import numpy as np
 
-driver = webdriver.Chrome()
 
-url = 'https://www.amazon.com'
+
+#url = 'https://www.amazon.com'
 
 
 def getUrl(search):
@@ -14,30 +14,67 @@ def getUrl(search):
     search = search.replace(' ', '+')
     return template.format(search)
 
-def cheapest(url):
-    
+def cheapest(item):
+    driver = webdriver.Chrome()
+    url = getUrl(item)
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    result = soup.find_all('div', {'data-component-type': 's-search-result'})
+
+    records = []
+    newPrice = []
+    for item in result:
+        record = extract_record(item)
+        if record:
+            if "Ounce" in record[2] or "Oz" in record[2]:
+                newAmount = filter(str.isdigit, record[2])
+                finalAmount = "".join(newAmount)
+                records.append(record)
+                newPrice.append(finalAmount)
 
 
-url = getUrl('sugar')
-driver.get(url)
+    newPrice = np.array(newPrice)
+    print(newPrice)
 
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-result = soup.find_all('div', {'data-component-type': 's-search-result'})
-'''print(len(result))
+    minimum = min(newPrice)
 
-item = result[0]
-atag = item.h2.a
-description = atag.text.strip()
-url = 'https://www.amazon.com' + atag.get('href')
+    #print(minimum)
 
-price_parent = item.find('span', 'a-price')
-price = price_parent.find('span', 'a-offscreen').text
+    for i in range(len(records)):
+        if newPrice[i] == minimum:
+            iteration = i
 
-rating = item.i.text
-print(rating)
+    final = records[iteration]
+    if "ounce" in records[iteration][4].lower():
+        res = records[iteration][4].lower().split("ounce")[0]
+        res = res.split()[-1]
 
-review_count = item.find('span', {'class': 'a-size-base', 'dir': 'auto'}).text
-print(review_count)'''
+    elif "pounds" in records[iteration][4].lower():
+        res = records[iteration][4].lower().split("pounds")[0]
+        res = res.split()[-1]
+        res = int(res) * 16
+    elif "lbs" in records[iteration][4].lower():
+        res = records[iteration][4].lower().split("lbs")[0]
+        res = res.split()[-1]
+        res = int(res) * 16
+    elif "lb" in records[iteration][4].lower():
+        res = records[iteration][4].lower().split("lb")[0]
+        res = res.split()[-1]
+        res = int(res) * 16
+    elif "fl" in records[iteration][4].lower():
+        res = records[iteration][4].lower().split("fl")[0]
+        res = res.split()[-1]
+    elif "oz" in records[iteration][4].lower():
+        res = records[iteration][4].lower().split("oz")[0]
+        res = res.split()[-1]
+
+
+    print(res)
+    print(final)
+
+
+    #print(final)
+
 
 def extract_record(item):
     atag = item.h2.a
@@ -69,54 +106,7 @@ def extract_record(item):
     return result
 
 
-records = []
-newPrice = []
-for item in result:
-    record = extract_record(item)
-    if record:
-        if "Ounce" in record[2]:
-            newAmount = filter(str.isdigit, record[2])
-            finalAmount = "".join(newAmount)
-            records.append(record)
-            newPrice.append(finalAmount)
 
 
-newPrice = np.array(newPrice)
-print(newPrice)
-
-minimum = min(newPrice)
-
-#print(minimum)
-
-for i in range(len(records)):
-    if newPrice[i] == minimum:
-        iteration = i
-
-final = records[iteration]
-if "ounce" in records[iteration][4].lower():
-    res = records[iteration][4].lower().split("ounce")[0]
-    res = res.split()[-1]
-
-elif "pounds" in records[iteration][4].lower():
-    res = records[iteration][4].lower().split("pounds")[0]
-    res = res.split()[-1]
-    res = int(res) * 16
-elif "lbs" in records[iteration][4].lower():
-    res = records[iteration][4].lower().split("lbs")[0]
-    res = res.split()[-1]
-    res = int(res) * 16
-elif "lb" in records[iteration][4].lower():
-    res = records[iteration][4].lower().split("lb")[0]
-    res = res.split()[-1]
-    res = int(res) * 16
-elif "oz" in records[iteration][4].lower():
-    res = records[iteration][4].lower().split("oz")[0]
-    res = res.split()[-1]
-
-
-print(res)
-print(final)
-
-
-#print(final)
-
+#cheapest('Sugar')
+cheapest('Syrup')
